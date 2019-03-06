ff4fb6093fd44eb1c9e9b70f6db58bd34e77d31c
Freddie Levenson <freddielev@gmail.com>
2019-03-05 17:33:41 -0700

    Create before, before each, and after for DB seed and rollback

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -7,3 +7,33 @@ const config = require('../knexfile')['test'];
 const database = require('knex')(config);
 chai.use(chaiHttp);
 
+describe('Server file', () => {
+    before(done => {
+        database.migrate
+            .rollback()
+            .then(() => database.migrate.latest())
+            .then(() => database.seed.run())
+            .then(() => done())
+            .catch(error => {
+                throw error;
+            })
+            .done();
+    })
+
+    beforeEach(done => {
+        database.migrate
+            .rollback()
+            .then(() => database.migrate.latest())
+            .then(() => database.seed.run())
+            .then(() => done());
+    });
+
+    after(done => {
+        database.migrate
+            .rollback()
+            .then(() => console.log('Testing complete. Db rolled back'))
+            .then(() => done());
+    })
+
+    
+})
